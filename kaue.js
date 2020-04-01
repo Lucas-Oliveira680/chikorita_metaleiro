68286c9edaf74c843c89ee0fca3eb94b07ddb04b
= <luccaseoliveira@gmail.com>
2020-03-31 21:24:56 -0300

    esquece o que eu disse

--- kaue.js
+++ kaue.js
@@ -7,5 +7,3 @@ setInterval(() => {
 
 var audio = document.getElementById("myaudio");
 audio.volume = 0.1;
-
-window.alert("A musica por algum motivo nao funciona no Chrome. A magia apenas ocorre sem ele :)")
