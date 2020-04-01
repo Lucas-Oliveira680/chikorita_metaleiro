6d8fef9ac00e6b31dd32c6a89418cc42c1eef718
= <luccaseoliveira@gmail.com>
2020-03-31 21:22:00 -0300

    Added Magic

--- kaue.js
+++ kaue.js
@@ -2,4 +2,10 @@ var spins = 0;
 setInterval(() => {
     spins += 1;
     document.querySelector("#counter").innerHTML = spins;
-},300)
+},350)
+
+
+var audio = document.getElementById("myaudio");
+audio.volume = 0.1;
+
+window.alert("A musica por algum motivo nao funciona no Chrome. A magia apenas ocorre sem ele :)")
